65fe9e951ea9ee9d43d0a666790047bfa3a06a2c
Charlene <charlenechetcuti@gmail.com>
2018-05-02 12:58:00 +0100

    add helper class

--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -1,17 +1,14 @@
-RSpec.feature "Entering players" do
+feature "Entering players" do
   scenario "Each can enter their names" do
-    visit "/"
-    fill_in('player1', :with => 'Charlene')
-    fill_in('player2', :with => 'Dione')
-    click_button 'Submit'
-    names = ("Player 1: Charlene vs. Player 2: Dione")
+    sign_in_and_play
+    names = ("Player 1: David vs. Player 2: Luke")
     expect(page).to have_text names
   end
 end
 
-RSpec.feature "Hit points" do
+feature "View Hit points" do
   scenario "viewing opponent's hit points" do
-    visit "/play"
+    sign_in_and_play
     expect(find('progress')['value']).to eq('40')
     expect(find('progress')['max']).to eq('100')
     expect(page).to have_text("Player 2's HP: 40")
