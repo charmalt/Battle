b40a006a1517bb66da3c3ceb6769b75d0f60c951
Charlene <charlenechetcuti@gmail.com>
2018-05-02 16:05:46 +0100

    refactored so that game class now instantiates the player instances

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,32 @@
 describe Game do
 
-  subject(:game) { described_class.new }
-  let(:player) { double :player, hit: nil}
+  subject(:game) { described_class.new(player1, player2) }
+  let(:player1) { double :player1 }
+  let(:player2) { double :player2 }
+
+  describe '#player1' do
+
+    it 'retrieves player 1' do
+      expect(game.player1).to be player1
+    end
+
+  end
+
+  describe '#player2' do
+
+    it 'retrieves player 1' do
+      expect(game.player2).to be player2
+    end
+
+  end
 
   describe '#attack' do
 
     it 'allows player to damage other player' do
-      expect(player).to receive(:hit)
-      game.attack(player)
+      expect(player2).to receive(:hit)
+      game.attack(player2)
     end
 
   end
+  
 end
